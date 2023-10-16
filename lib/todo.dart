a030cc31ac831f51ad616091c9aa52ea7851628d
AA <yamafuji03@gmail.com>
2023-10-16 12:43:26 +0900

    説明変更

--- lib/todo.dart
+++ lib/todo.dart
@@ -130,18 +130,17 @@ class _TodoState extends State<Todo> {
           // 動かすドキュメントIDを取得
           final moveId = snapshot.data!.docs[oldIndex].id;
 
-          // ここでforでそれ以外のorderをマイナス１にする
-          // 始まりを「１」とするのはorderの値をマイナス１してることから１始まりにしないとorder番号がマイナスになる可能性が出るから
+          // ここでforでそれ以外のorderをプラス１にする
+          // 始まりを「newIndex」として仮に3から2へ移動する際にorder1には影響が出ないようにする
           for (int i = newIndex; i <= oldIndex; i = i + 1) {
             // 他のドキュメントのIDを取得
             final otherId = snapshot.data!.docs[i].id;
-            // 移動させたリストと古いリストのorderが被っているから、もし移動したIDとiのIDが違うなら処理を実行とする。
+            // 移動させたリストと古いリストのorderが被ってないなら、orderをプラス１して並び替える。
             if (moveId != otherId) {
               FirebaseFirestore.instance
                   .collection(widget.user.email!)
                   .doc(otherId)
                   .update({
-                // orderをi-1にして選択されていないリストの中にあるorderを１ずらす
                 'order': i + 1,
               });
             }
@@ -151,7 +150,7 @@ class _TodoState extends State<Todo> {
               .collection(widget.user.email!)
               .doc(moveId)
               .update({
-            // newIndexだと最大値プラス１が取れてしまうため、マイナス１で移動先リストと同じindexになるように調整
+            // newIndexをそのままorder番号にする
             'order': newIndex,
           });
         }
